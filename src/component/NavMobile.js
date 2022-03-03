3520df7d4624e7c6fb665a0e684bfc8cfe71a8e4
abbos0959 <abbosakromovich777@gmail.com>
2022-03-03 22:05:08 +0500

    card

---
+++ src/component/NavMobile.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const NavMobile = () => {
+  return (
+    <nav class="humberger__menu__nav mobile-menu">
+    <ul>
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">Shop</a></li>
+        <li><a href="#">Pages</a>
+           
+        </li>
+        <li><a href="#">Blog</a></li>
+        <li><a href="#">Contact</a></li>
+    </ul>
+</nav>
+    
+  )
+}
